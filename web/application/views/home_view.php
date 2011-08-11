f735b9d0cff0e34e8de1f3ab59c32f88dbc8fbdf
Kazuna Nakama <kazunanakama@gmail.com>
2011-08-11 01:58:50 -0500

    DERP.

--- web/application/views/home_view.php
+++ web/application/views/home_view.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Title Here</title>
+	<title>VGdesk</title>
 	<head>
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
 		<!-- The Following Script Contains all of the AJAX Page Loading Logic -->
 		<script type="text/javascript" src="js/navigation.js"></script>
 		
@@ -15,16 +16,25 @@
 	</head>
 </head>
 
-<div>
-	Header
+<div id="header">
+	<img id="logo" src="assets/css/images/logo.png"/>
+	<p class="header-button"><a href="#">Overview</a></p>
+	<p class="header-button"><a href="#">Files</a></p>
+	<p class="header-button"><a href="#">Planner</a></p>
+	<p class="header-button"><a href="#">Writer</a></p>
+	<p class="header-button"><a href="#">Forum</a></p>
+	<p class="header-button"><a href="#">Chat</a></p>
+	<p class="header-button"><a href="#">Tools</a></p>
 </div>
 
-<div id="pageBody">
+<div id="page-body">
 <!-- This is where the loaded pages will be dumped -->
 </div>
 
-<div>
-	Footer
+<div id="footer">
+	<div id="arrow-left">::</div>
+	<div class="footer-content"></div>
+	<div id="arrow-right">::</div>
 </div>
 
 </html>
