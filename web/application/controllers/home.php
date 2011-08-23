828ef7c3abd9bae0ac592c94293a513290d624eb
Xander Masotto <xmasotto@gmail.com>
2011-08-23 01:40:10 -0700

    $user now works in home_view.php

--- web/application/controllers/home.php
+++ web/application/controllers/home.php
@@ -1,7 +1,22 @@
+
 <?php
 
 class Home extends CI_Controller 
 {
+    protected $user;
+
+    public function __construct()
+    {
+	parent::__construct();
+	
+	// no cache
+        header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+        header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+	$this->load->model("login/login_model");
+	$user = $this->login_model->getLoggedInUser();
+    }
+
     protected function rebuildGet()
     {
 	parse_str($_SERVER['QUERY_STRING'], $_GET);
@@ -11,6 +26,7 @@ class Home extends CI_Controller
     {
 	$data = array();
 	$data["default_page"] = $pageId;
+	$data["user"] = $this->user;
 	$this->load->view("home_view", $data);
     }
 
