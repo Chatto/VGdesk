4fa5d7f2c8145f52ba6e3c288eecee1c5cf461b6
Kazuna Nakama <kazunanakama@gmail.com>
2011-08-23 20:04:47 -0500

    Added a simple registration box. Also subtly cleaned up the CSS. I need to find a way to rewrite that one nasty selector for those forms. I also need to consider adding a generic .right{float:right;} type of class. Javascript to be added to login/registration page.

--- web/application/views/login_view.php
+++ web/application/views/login_view.php
@@ -40,38 +40,45 @@
 </span>
 
 <div id="login-body">
-		<div class="section" id="login-box">
-			<div class="section-header">Login to Travels of Oorta</div>
-			
-			<div id="login-forms">
-				<img class="avatar-border project-image" src="assets/images/project-image.png"/>
-				<form action="javascript:LoginView.onLogin()" method="post">
-					<fieldset>
-						<label for="username">Username</label>
-							<input id="username-input" type="text" name="username"/>
-						<label for="password">Password</label>
-							<input id="password-input" type="password" name="password" /><br>
-						
-						<input type="submit" value=" Log On&#8594;" />
-						<input type="button" value=" Register " />
-						</fieldset>
-							
-				</form>
-			</div>
-		<?php
-			if ($user != null)
-			{
-				echo "Current Logged In User: " . $user->username; //should go to home_view instead
-			}
-			else
-			{
-				echo "";
-			}
-		?>
+	<div class="section" id="login-box">
+		<div class="section-header">
+			Login to Travels of Oorta
 		</div>
-</div>
+		<div id="login-forms">
+			<img class="avatar-border project-image" src="assets/images/project-image.png"/>
+			<form action="javascript:LoginView.onLogin()" method="post">
+				<fieldset>
+					<label for="username">Username</label>
+					<input id="username-input" type="text" name="username"/>
+					<label for="password">Password</label>
+					<input id="password-input" type="password" name="password" /><br>						
+					<input type="submit" value=" Log On&#8594; " />
+					<input type="button" value=" Register " />
+				</fieldset>			
+			</form>
+		</div>
+	</div>
 
-<div id="registration-box">
-	
-
+	<div class="section" id="reg-box">
+		<div class="section-header">
+			Travels of Oorta Registration
+		</div>
+		<div id="reg-forms">
+			<form>
+				<fieldset>
+					<label for="regdisplayname">Display Name</label>
+					<input id="reg-displayname" type="text" name="regdisplayname"/>
+					<label for="regusername">Username</label>
+					<input id="reg-displayname" type="text" name="regusername"/>
+					<label for="regpass">Password</label>
+					<input id="reg-pass" type="password" name="regpass"/>
+					<label for="regpassverify">Re-type Password</label>
+					<input id="reg-passverify" type="password" name="regpassverify"/>
+					<label for="regemail">Email</label>
+					<input id="reg-email" type="text" name="regemail"/><br>
+					<input type="submit" value=" Submit&#8594; ">
+				</fieldset>
+			</form>
+		</div>
+	</div>
 </div>
